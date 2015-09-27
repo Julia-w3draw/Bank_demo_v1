43216289bcf9c31a790cf88d6d8502f999525e0b
Julia <Julia-w3draw@users.noreply.github.com>
2015-09-27 20:10:47 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,205 @@
+<html lang="en-US">
+	<head>
+		<meta charset="UTF-8">
+		<title>Bank Exercise</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="keywords" content="bank">
+		<meta name="description" content="Bank Exercise">
+		<meta name="author" content="Julia Dascalu">
+		<link rel="stylesheet" type="text/css" href="style.css">			
+	</head>
+	
+	<body>
+	<!--_page_wp:-->
+	<div id='_page_wp'>
+		<div id='_top_spacer'></div>
+		<!--_top_menu:-->
+		<div id='_top_menu' >
+			<!--_top_menu.left:-->
+			<div id='_top_menu_left'>			
+				<!--_logo:-->
+				<img id='_logo' src='IMG/ICONS/bankofme_small.png'>
+				<!--:_logo-->				
+				<!--_left_home:-->
+				<img id='_left_home' src='IMG/ICONS/smallhomegrey.png' >
+				<!--:_left_home-->
+			</div><!--_top_menu.left:-->
+			
+			<!---_top_menu_right:-->
+			<div id='_top_menu_right' >
+				<!--_mobile_b:-->
+				<img class='_top_menu_right_b' src='IMG/ICONS/smallmobile.png' >
+				<!--:_mobile_b-->
+				<!--_account_b:-->
+				<img class='_top_menu_right_b' src='IMG/ICONS/smallmegrey.png'>
+				<!--:_account_b-->
+				<!--_zoom_b:-->
+				<img class='_top_menu_right_b' src='IMG/ICONS/magnifier.png'>
+				<!--:_zoom_b-->
+				<!--_edit_b:-->
+				<img class='_top_menu_right_b' src='IMG/ICONS/pen.png' >
+				<!--:_edit_b-->
+				<img id='_mobile_menu' src='IMG/ICONS/hamburger.png' >
+			</div><!--_top_menu.right:-->
+		</div><!--:_top_menu-->
+		
+		<!--_middle_menu:-->
+		<div id='_middle_menu' >
+			<!--_middle_menu_center:-->
+			<div id='_middle_menu_center'>
+				<!--_home_b:-->
+				<div class='_middle_menu_b'>
+					<img src='IMG/ICONS/smallhome.PNG' >
+					<div class="_txt_b">Home</div>
+				</div>				
+				<!--:_home_b-->
+				<!--_connect_b:-->
+				<div class='_middle_menu_b'>
+					<img src='IMG/ICONS/link.PNG'>
+						<div class="_txt_b">Connect</div>
+				</div>
+				<!--:_connect_b-->
+				<!--_discover_b:-->
+				<div class='_middle_menu_b'>
+					<img src='IMG/ICONS/discover.png'>
+						<div class="_txt_b">Discover</div>
+				</div>
+				<!--:_discover_b-->
+				<!--_me_b:-->
+				<div class='_middle_menu_b' style='border-right: 1px solid #ffffff;'>
+					<img src='IMG/ICONS/smallme.PNG'>
+						<div class="_txt_b">Me</div>
+				</div>
+				<!--:_me_b-->
+			</div><!--:_middle_menu.center-->
+		</div><!--:_middle_menu-->
+
+		<!--_content:-->
+		<div id='_content'>			
+			<!--_message_wp:-->
+			<div id='_message_wp'>
+			<p id='_title_1'>Get Your Student Bank Account In Minutes!</p>	
+			<p id='_message'>Save time by using your camera phone to send us a proof of address and selfie.<br><br>We'll also need your UCAS student number, so make sure you have it to hand.</p>		
+			</div><!--:_message_wp-->
+		</div><!--:_content-->
+		
+		<!--_bottom_menu:-->
+		<div id='_bottom_menu_1'>
+		<!--_bottom_menu_center:-->
+			<div id='_bottom_menu_center_1'>
+				<div id='_video_b' class="_bottom_menu_b" >VIDEO CALL</div>
+				<div style='width:15px'></div>
+				<div id='_chat' class="_bottom_menu_b">LIVE CHAT</div>
+		</div><!--_bottom_menu_center:-->				
+		</div><!--:_bottom_menu-->
+		
+		<!--_bottom_menu:-->
+		<div id='_bottom_menu_2'>
+		<!--_bottom_menu_center:-->
+			<div id='_bottom_menu_center_2'>
+				<div id='visit_b' class="_bottom_menu_b" >VISIT BRANCH</div>			
+		</div><!--_bottom_menu_center:-->				
+		</div><!--:_bottom_menu-->
+		</div><!--:_page_wp-->
+	
+	<!--_p_chat_window:-->
+		<div id='_p_chat_window' style=''>
+		<!--_chat_top:-->
+			<div id='_chat_top'>
+			<!--_chat_top_left:-->
+			<div id='_chat_top_left'>
+				<img src="IMG/ICONS/chat.png">
+			</div><!--:_chat_top_left-->
+			<div id='_chat_top_right'>
+				<img src="IMG/ICONS/video.png">
+			</div>				
+			</div><!--:_chat_top-->
+			
+			<!--	_chat_middle:-->
+			<div id='_chat_middle'>
+				<!--operator_section:-->
+				<div id='_operator_section'>
+					<!--_operator_img:-->
+					<div id='_operator_img_wp'>
+						<img class="img-circle" src="IMG/ICONS/agent_chat.png">
+					</div><!--:_operator_img-->
+					<!--_operator_center_section:-->
+					<div id='_operator_center_section'>
+						<div id='_operator_name'>Joe</div>
+						<div id='_operator_message'>
+							Aliquam nec commodo elit. Nunc dignissim pharetra purus ut malesuada. Suspendisse pharetra leo diam. Aliquam ultrices turpis malesuada sem elementum scelerisque. Nunc interdum ex non diam interdum dictum. Phasellus eget venenatis ipsum. 
+						</div>
+					</div><!--:_operator_center_section-->
+					<!--_operator_right_section:-->
+					<div id='_operator_right_section'>
+						<div id='_operator_clock'>
+							<img src='IMG/ICONS/clock.png'>
+						</div>
+						<div id='_oprator_time'>15 min ago</div>
+					</div><!--:_operator_right_section-->
+				</div><!--:operator_section-->
+		
+				<!--client_section:-->
+				<div id='_client_section'>
+					<!--_client_left_section:-->
+					<div id='_client_left_section'>
+						<div id='_client_clock'>
+							<img src='IMG/ICONS/clock.png'>
+						</div>	
+						<div id='_client_time'>13 min ago</div>
+						<!-- _client_message:-->
+						<div id='_client_message'>
+							Praesent vitae fermentum libero, vel tempus felis. Phasellus suscipit finibus felis vitae mattis. Etiam erat odio, volutpat ac eleifend eu, tristique ut diam. Integer auctor euismod diam, nec efficitur sem feugiat in. 
+						</div><!-- :_client_message-->
+					</div><!--:_client_left_section-->
+					<!--_client_right_section:-->
+					<div id='_client_right_section'>
+						<div id='_client_name'>You</div>
+						<div id='_client_img'>
+							<img class="img-circle" src="IMG/ICONS/client_chat.png">
+						</div>
+					</div><!--:_client_right_section-->
+				</div><!--:client_section-->				
+			</div><!--:_chat_middle-->		
+		
+		<div id='_chat_bottom'>
+		<!--_chat_bottom_left:-->
+			<div id='_chat_bottom_left'>
+				<div id='_upload_file'>
+					<img src="IMG/ICONS/folder.png">
+				</div>
+				<div id='_upload_img'>
+					<img src="IMG/ICONS/camera.png">
+				</div>
+			</div><!--:_chat_bottom_left-->
+			<!--_chat_bottom_center:-->
+			<div id='_chat_bottom_center'>
+				<form action="_chat.php">
+					<input id='_message_input' type="text" name="fname" placeholder="Type your message here...">
+					<input id='_submit_b' type="submit" value="SEND MESSAGE">
+				 </form>
+			</div><!--:_chat_bottom_center-->
+		</div><!--_:chat_bottom_right-->
+		</div><!--:_p_chat_window-->
+		
+	</body>
+	
+	<script>		
+		var _chat_b=document.getElementById('_chat')
+		var _content=document.getElementById('_content')
+		var _bottom_menu_1=document.getElementById('_bottom_menu_1')
+		var _bottom_menu_2=document.getElementById('_bottom_menu_2')
+		var _p_chat_window=document.getElementById('_p_chat_window')
+		
+		_chat_b.onclick=function(){
+			document.body.style.backgroundImage='none'
+			_content.style.display='none'
+			_bottom_menu_1.style.display='none'
+			_bottom_menu_2.style.display='none'
+			_p_chat_window.style.display='block'
+		}		
+	</script>
+</html>
+
+
+
